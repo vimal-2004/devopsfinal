785742c12d9e77d341a2fe4e4c018f645a261eae
vimal-2004 <148618715+vimal-2004@users.noreply.github.com>
2025-04-04 10:48:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker build -t task2 .
 echo Hyyy
 docker login -u vimal2004 -p 22ISR059@
-docker tag task2 vimal2004/devops
-docker push vimal2004/devops
+docker tag task2 vimal2004/final_task
+docker push vimal2004/final_task
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
